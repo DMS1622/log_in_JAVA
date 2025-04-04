f2663572b3ebf74edeb1f34af2f167de163f440d
DMS1622 <diego.sanchez2004@outlook.es>
2025-04-04 19:10:19 -0300

    pc

--- app/src/main/java/com/example/log_in/home/home.java
+++ app/src/main/java/com/example/log_in/home/home.java
@@ -3,6 +3,7 @@ package com.example.log_in.home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.log_in.R;
@@ -29,7 +30,7 @@ public class home extends AppCompatActivity {
 
         cad = user.getText().toString();
         cad = cad+usr;
-        user.setText(cad);
+        Toast.makeText(this, cad, Toast.LENGTH_SHORT).show();
 
 
     }
